40e403d2a2124776169dfa5d983d70cb465df2bb
Caseisbusy <86605082+caseisbusy@users.noreply.github.com>
2022-11-17 08:41:32 +0000

    update files

--- js/discordFinder.js
+++ js/discordFinder.js
@@ -10,13 +10,39 @@ form.addEventListener("submit", function(e){
   let on = search.split(' ').join("")
 
 fetch(`https://japi.rest/discord/v1/user/${on}`).then(response => response.json()).then(data => {
+	let badgestops = {
+    HOUSE_BRAVERY: "HypeSquad Bravery",
+    HOUSE_BRILLIANCE: "HypeSquad Brilliance",
+    HOUSE_BALANCE: "HypeSquad Balance",
+    BUGHUNTER_LEVEL_1: "BugHunter_lvl(1)",
+    BUGHUNTER_LEVEL_2: "BugHunter_lvl(2)",
+    HYPESQUAD_EVENTS: "Hypesquad Event",
+    EARLY_VERIFIED_DEVELOPER: "Early Verified Developer",
+    VERIFIED_DEVELOPER: "Verified Developer",
+    DISCORD_PARTNER: "Discord Partner",
+    PARTNERED_SERVER_OWNER: "Server Partner",
+    EARLY_SUPPORTER: "Early Supporter",
+    DISCORD_EMPLOYEE: "Discord Employee",
+		ACTIVE_DEVELOPER: "Active Developer"
+	};
+	  let badges = data.data.public_flags_array
+	
+let timestamp = data.data.createdTimestamp
+let date = new Date(timestamp)
+let fullDate = date.getDate()+
+          "/"+(date.getMonth()+1)+
+          "/"+date.getFullYear()+
+          " "+date.getHours()+
+          ":"+date.getMinutes()+
+          ":"+date.getSeconds()
 let dataDocument = document.getElementById("result").innerHTML = `
 <link href="css/discordFinder.css" rel="stylesheet" type="text/css"/>
 <a target="_blank" href="https://discord.com/users/${on}"><img src="${data.data.avatarURL}?size=256" alt="Users avatar"/></a>
 <div class="res">Username: ${data.data.tag || "None"}</div>
 <div class="res">About me: ${data.data.bio || "None"}</div>
-<div class="res">Created: ${data.data.createdAt}</div>
-<div class="res">Badges: ${data.data.public_flags_array}</div>`
+<div class="res">Created: ${fullDate}</div>
+<div class="res">Badges: ${badges.length ? badges.map(flag => badgestops[flag]).join(", ") : "None" }</div>
+`
 
 if(!data.data.username) {
 	dataDocument = document.getElementById("result").innerHTML = `No data to show, user not found`
