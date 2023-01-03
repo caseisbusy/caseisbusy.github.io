d99affb0f05e0e3ede9e1f2b19f51bc91d41dc0c
Caseisbusy <86605082+caseisbusy@users.noreply.github.com>
2023-01-03 07:11:24 +0000

    a

--- js/githubUserFinder.js
+++ js/githubUserFinder.js
@@ -10,13 +10,16 @@ fetch(`https://api.github.com/users/${on}`).then(response => response.json()).th
 console.log(data) 
 let dataDocument = document.getElementById("result").innerHTML = `
 <link href="css/githubUserFinder.css" rel="stylesheet" type="text/css"/>
+<a target="_blank" href="https://github.com/${on}"><div class="parent"><img class="upfp" src="${data.avatar_url}?size=256" alt="Users avatar"/></a><div><p class="res">Name: ${data.name || "None"}</p><p class="res">Bio: ${data.bio || "None"}</p><p class="res">Followers: ${data.followers}</p><p class="res">Public Repos: ${data.public_repos}</p><p class="res">Company: ${data.company || "No company"}</p></div></div>
+`/*`
+<link href="css/githubUserFinder.css" rel="stylesheet" type="text/css"/>
 <a target="_blank" href="https://github.com/${on}"><img src="${data.avatar_url}"/></a>
 <div class="res">Name: ${data.name || "None"}</div>
 <div class="res">Bio: ${data.bio || "None"}</div>
 <div class="res">Followers: ${data.followers}</div>
 <div class="res">Public Repos: ${data.public_repos}</div>
 <div class="res">Company: ${data.company || "No Company"}</div>
-`
+`*/
 if(!data.avatar_url) {
 	dataDocument = document.getElementById("result").innerHTML = `No data to show, user not found`
 }
